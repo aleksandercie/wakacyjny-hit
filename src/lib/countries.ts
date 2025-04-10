1c633ef81f9b5709777f8a3a58b3479a8a85ff42
AGasior <aleksander.gasior@10clouds.com>
2025-04-10 18:26:58 +0200

    update supported countries

--- src/lib/countries.ts
+++ src/lib/countries.ts
@@ -1,12 +1,11 @@
 export const countries = [
   { label: 'Austria', value: 'AT' },
   { label: 'Czechy', value: 'CZ' },
+  { label: 'Dania', value: 'DK' },
   { label: 'Holandia', value: 'NL' },
   { label: 'Niemcy', value: 'DE' },
   { label: 'Norwegia', value: 'NO' },
-  { label: 'Polska', value: 'PL' },
-  { label: 'Słowacja', value: 'SK' },
+  { label: 'Szwajcaria', value: 'CH' },
   { label: 'Szwecja', value: 'SE' },
-  { label: 'Ukraina', value: 'UA' },
   { label: 'Wielka Brytania', value: 'GB' }
 ];
