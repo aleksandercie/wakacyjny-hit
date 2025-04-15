1e2424ff59ad0bc5a2a37cb7a9a3e23e479481d4
AGasior <aleksander.gasior@10clouds.com>
2025-04-15 20:45:41 +0200

    add cta to video banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Video } from '../video';
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export const Banner = ({
   video,
@@ -15,11 +17,14 @@ export const Banner = ({
   return (
     <div className="w-full m-auto">
       <div className="relative">
-        <div className="absolute top-[90px] min-[500px]:top-3/5 min-[720px]:top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 text-white text-center max-w-[700px] z-1 w-full px-4">
-          <h1 className="text-xl xs:text-2xl sm:text-4xl md:text-6xl font-bold mb-3 sm:mb-6 w-full mx-auto">
+        <div className="flex flex-col gap-3 sm:gap-6 absolute top-[90px] min-[500px]:top-3/5 min-[720px]:top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 text-white text-center max-w-[700px] z-1 w-full px-4">
+          <h1 className="text-xl xs:text-2xl sm:text-4xl md:text-6xl font-bold w-full mx-auto">
             {title}
           </h1>
           <p className="text-s xs:text-m sm:text-l md:text-xl">{description}</p>
+          <Link href="/oferty">
+            <Button>Sprawdź</Button>
+          </Link>
         </div>
         <div className="w-full h-[600px] md:h-auto rounded-md overflow-hidden">
           <Video videoSrc={video} videoSrcMobile={videoMobile} />
