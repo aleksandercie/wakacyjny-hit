911a96f2c570cb62f6201b68a268dd0f7079fe39
aleksandercie <aleksander.cieszyn@gmail.com>
2025-04-29 20:07:01 +0200

    social links add aria label

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -4,22 +4,24 @@ import Link from 'next/link';
 
 export const Contact = ({ title }: { title: string }) => {
   const contact = [
-    { link: 'facebook', icon: <Facebook /> },
-    { link: 'instagram', icon: <Instagram /> },
-    { link: 'mail', icon: <Mail /> }
+    { link: 'facebook', icon: <Facebook />, name: 'Facebook' },
+    { link: 'instagram', icon: <Instagram />, name: 'Intagram' },
+    { link: 'mail', icon: <Mail />, name: 'Mail' }
   ];
 
   return (
     <div className="flex flex-col gap-4">
       <h4 className="text-xl font-bold">{title}</h4>
-      <p className="text-base text-gray-500">+48 800 800 800 900</p>
+      <p className="text-base text-gray-500">+48 800 800 800</p>
       <p className="text-base text-gray-500">info@wakacyjnyhit.pl</p>
       <div className="flex gap-4">
-        {contact.map(({ link, icon }) => (
+        {contact.map(({ link, icon, name }) => (
           <Link
             key={link}
             href={link}
             className="text-sm hover:text-primary focus:text-primary"
+            target="_blank"
+            aria-label={name}
           >
             {icon}
           </Link>
